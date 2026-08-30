0864f67d7efbe30ab0da39fdd9dc31382f6f12e4
Invictus-GR <georgetsim29@gmail.com>
2026-08-30 17:23:15 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -294,7 +294,7 @@ async function sendDailyReport() {
       SELECT *
       FROM daily_stats
       WHERE report_date =
-        (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/London')::date - 1
+        (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/London')::date
     `);
 
     if (result.rows.length === 0) {
@@ -1165,14 +1165,15 @@ if (isShowMods) {
   }
 });
 client.once('clientReady', async () => {
-  await testDatabaseConnection();
-  await initializeDatabase();
+await testDatabaseConnection();
+await initializeDatabase();
+await sendDailyReport();
 
-  cron.schedule('5 0 * * *', async () => {
-    await sendDailyReport();
-  }, {
-    timezone: 'Europe/London'
-  });
+cron.schedule('5 0 * * *', async () => {
+  await sendDailyReport();
+}, {
+  timezone: 'Europe/London'
+});
   
   await client.application.commands.set([]);
 
